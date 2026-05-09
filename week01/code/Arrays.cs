98237a6be56cff7ba5579af8316c3290db17b210
Chelsea1246 <chelseaopoku9@gmail.com>
2026-05-09 15:53:54 -0700

    Implement logic for MultiplesOf method and complete RotateListRight functionality

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,20 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Creating an array that will store the multiples of the number.  The size of the array is determined by the length parameter.
+        double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+         // Loop through each position in the array
+        for (int i = 0; i < length; i++)
+        {
+            // Multiply the number by (i + 1)
+            // i starts at 0, so adding 1 gives us:
+            // 1, 2, 3, 4, ...
+            multiples[i] = number * (i + 1);
+        }
+
+        // Return the completed array
+        return multiples; 
     }
 
     /// <summary>
@@ -25,9 +33,20 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+         // Find the position where the list should be split
+        int splitIndex = data.Count - amount;
+
+        // Get the last portion of the list that will move to the front
+        List<int> endPart = data.GetRange(splitIndex, amount);
+
+        // Get the beginning portion of the list
+        List<int> frontPart = data.GetRange(0, splitIndex);
+
+        // Clear the original list
+        data.Clear();
+
+        // Add the rotated sections back into the list
+        data.AddRange(endPart);
+        data.AddRange(frontPart);
     }
 }
