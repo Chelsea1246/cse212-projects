5aab5822d88958ba1fc4f59201b8d7623f1d54ff
Chelsea1246 <chelseaopoku9@gmail.com>
2026-06-13 15:45:41 -0700

    Implement backward traversal in BinarySearchTree and enhance Node methods for unique value insertion and height calculation

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,11 +11,13 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        // Problem 1: only allow unique values
+
+        if (value == Data)
+            return;
 
         if (value < Data)
         {
-            // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
@@ -23,7 +25,6 @@ public class Node
         }
         else
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -33,13 +34,34 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        // Problem 2
+
+        if (value == Data)
+            return true;
+
+        if (value < Data)
+        {
+            if (Left is null)
+                return false;
+
+            return Left.Contains(value);
+        }
+        else
+        {
+            if (Right is null)
+                return false;
+
+            return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // Problem 4
+
+        int leftHeight = Left == null ? 0 : Left.GetHeight();
+        int rightHeight = Right == null ? 0 : Right.GetHeight();
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
